f8099467cc81d36208e3d491037d0d47ff1ac202
Albert0002 <albert.lannerstrom@gmail.com>
2023-05-06 13:16:53 +0200

    Copied lab4 to lab5

---
+++ Labs/Lab5/GLprimer/fragment.glsl
@@ -0,0 +1,43 @@
+#version 330 core
+
+uniform float time;
+
+in vec3 interpolatedNormal;
+in vec2 st;
+
+out vec4 finalcolor;
+in vec3 lightDirection;
+
+vec3 Ia = vec3(0.0, 0.0, 1.0);
+vec3 ka = vec3(0.0, 0.0, 1.0);
+vec3 Id = vec3(0.75, 0.75, 0.75);
+vec3 kd = vec3(0.75, 0.75, 0.75);
+vec3 Is = vec3(1.0, 1.0, 1.0);
+vec3 ks = vec3(1.0, 1.0, 1.0);
+float n = 20;
+
+void main() {
+	//finalcolor = vec4(1.0, 1.0, 1.0, 1.0);
+	//finalcolor = vec4(interpolatedNormal * 0.5 + 0.5, 1.0);
+	
+	/*
+	vec3 R = 2.0 * dot(interpolatedNormal, lightDirection) * interpolatedNormal - lightDirection;
+	float dotRV = max(dot(R, vec3(0.0, 0.0, 1.0)), 0.0);
+	float dotNL = max(dot(interpolatedNormal, lightDirection), 0.0);
+	if (dotNL == 0.0) {
+		dotRV == 0.0;
+	}
+	vec3 shadedcolor = Ia * ka + Id * kd * dotNL + Is * ks * pow(dotRV, n);
+	*/
+
+	float dotNL = max(dot(interpolatedNormal, lightDirection), 0.0);
+	vec3 shadedcolor = Ia * ka + Id * kd * dotNL;
+
+	finalcolor = vec4(shadedcolor, 1.0);
+	
+	//float shading = dot(interpolatedNormal, lightDirection);
+	//shading = max(0.0, shading); //Clamp negative values to 0.0
+	//finalcolor = vec4(vec3(shading), 1.0);
+	//vec3 normalizedNormal = normalize(interpolatedNormal);
+	//finalcolor = vec4(vec3(normalizedNormal.z), 1.0);
+}
---
+++ Labs/Lab5/GLprimer/vertex.glsl
@@ -0,0 +1,34 @@
+#version 330 core
+
+uniform float time;
+
+/*
+uniform mat4 Ry;
+uniform mat4 Rx;
+uniform mat4 T;
+uniform mat4 Rz;
+*/
+
+uniform mat4 MV;
+uniform mat3 MV_normal;
+uniform mat4 P;
+
+layout(location = 0) in vec3 Position;
+layout(location = 1) in vec3 Normal;
+layout(location = 2) in vec2 TexCoord;
+
+out vec3 interpolatedNormal;
+out vec2 st;
+out vec3 lightDirection;
+
+void main() {
+	lightDirection = vec3(0.0, 0.0, 1.0);
+	gl_Position = P * MV * vec4(Position, 1); // Special, required output
+
+	//vec3 MV_norm = vec3(MV);
+
+	// ??????
+	vec3 transformedNormal = transpose(inverse(mat3(P * MV))) * Normal; //mat3(Ry * Rx) * Normal;
+	interpolatedNormal = normalize(transformedNormal);
+	st = TexCoord; // Will also be interpolated across the triangle
+}
